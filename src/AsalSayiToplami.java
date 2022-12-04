c5556a47e4adf33b8fef3380548793c8d0f11431
{ismailsari} <{ismailsari344@gmail.com}>
2022-12-04 18:49:16 +0300

    19th practice

--- src/AsalSayiToplami.java
+++ src/AsalSayiToplami.java
@@ -7,10 +7,14 @@ public class AsalSayiToplami {
 
         // 1'den 100'e kadar olan asal sayilar toplamini yazdirin
 
-        int asalToplami=16;
+        int asalToplami=0;
         List<Integer> asallar=new ArrayList<>();
+        asallar.add(2);
+        asallar.add(3);
+        asallar.add(5);
+        asallar.add(7);
 
-        for (int i = 99; i >=1; i--) {
+        for (int i = 2; i <100; i++) {
             if (i%2!=0 && i%3!=0 && i%7!=0 && i%5!=0){
                 asallar.add(i);
             }
